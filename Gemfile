0316c5238e32ad87cb08b9e7ca1851899736089a
Ivan Rodriguez <gatopan@icloud.com>
2015-04-15 19:07:29 -0700

    added cors with allow all rule

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'rails-api'
 gem 'bcrypt', '~> 3.1.7'
 gem 'active_model_serializers'
 
+# Middleware
+gem 'rack-cors', '0.2.9', require: 'rack/cors'
+
 group :production do
   gem 'pg'
 end
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,17 @@ module AngularApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # configure Rack::Cors
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', {
+          headers: :any,
+          methods: [:get, :post, :options, :put, :patch, :delete]
+        }
+      end
+    end
+
   end
 end
